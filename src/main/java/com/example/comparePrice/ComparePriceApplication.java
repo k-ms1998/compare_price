14a882b4f6a5021b506ebff8d9db86cab52822b2
Min Seop Kim <k_ms1998@naver.com>
2023-03-28 23:22:19 +0900

    Initial Setup

---
+++ src/main/java/com/example/comparePrice/ComparePriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.comparePrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparePriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparePriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comparePrice/ComparePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comparePrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparePriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
